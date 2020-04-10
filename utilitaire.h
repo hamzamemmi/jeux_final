dff677b499129b912e5404076ebc0106da6c97e8
firaschkoundali <firas.chkoundali@esprit.tn>
2020-04-10 14:53:31 -0700

    pour le moment elle contient seulement la structure texte

--- utilitaire.h
+++ utilitaire.h
@@ -1,16 +1,21 @@
 #ifndef UTILITAIRE_H_INCLUDED
 #define UTILITAIRE_H_INCLUDED
-
+#include "perso.h"
 typedef struct
 {	SDL_Color couleur;
 	SDL_Surface *texte;
 	TTF_Font *police=NULL;
-	SDL_Rect postexte;
+	SDL_Rect posvie;
 }texte;
-void initialiservie(texte *textevie);
-void initialiserscore(texte *textescore);
-void initialisertemps(texte *t);
 
-
+void initialiservie(texte *textevie ,int *vie);
+void initialiserscore(texte *textescore,int score);
+void initialisertemps(texte *temps);
+void miseajourvie(texte *textevie, int *vie);
+void miseajourcore(texte *textescore,int *score);
+void miseajourtemps(texte *temps);
+void affichervie(texte textevie,int vie);
+void afficherscore(texte textescore,int score);
+void affichertemps(texte temps);
 
 #endif
