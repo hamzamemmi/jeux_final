beef49aa1e6937c36d0d2b5fbe0ee116cd12efda
firaschkoundali <62991349+firaschkoundali@users.noreply.github.com>
2020-04-10 14:57:08 -0700

    Update utilitaire.h

--- utilitaire.h
+++ utilitaire.h
@@ -1,6 +1,6 @@
 #ifndef UTILITAIRE_H_INCLUDED
 #define UTILITAIRE_H_INCLUDED
-#include "perso.h"
+
 typedef struct
 {	SDL_Color couleur;
 	SDL_Surface *texte;
@@ -8,14 +8,6 @@ typedef struct
 	SDL_Rect posvie;
 }texte;
 
-void initialiservie(texte *textevie ,int *vie);
-void initialiserscore(texte *textescore,int score);
-void initialisertemps(texte *temps);
-void miseajourvie(texte *textevie, int *vie);
-void miseajourcore(texte *textescore,int *score);
-void miseajourtemps(texte *temps);
-void affichervie(texte textevie,int vie);
-void afficherscore(texte textescore,int score);
-void affichertemps(texte temps);
+
 
 #endif
