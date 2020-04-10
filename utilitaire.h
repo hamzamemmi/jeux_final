64e2fbf3224f703808066a6094f62c8f679a0506
firaschkoundali <firas.chkoundali@esprit.tn>
2020-04-10 14:05:44 -0700

    pour le moment elle contient seulement la structure texte

---
+++ utilitaire.h
@@ -0,0 +1,14 @@
+#ifndef UTILITAIRE_H_INCLUDED
+#define UTILITAIRE_H_INCLUDED
+
+typedef struct
+{	SDL_Color couleur;
+	SDL_Surface *texte;
+	TTF_Font *police=NULL;
+	SDL_Rect posvie;
+}texte;
+
+
+
+
+#endif
